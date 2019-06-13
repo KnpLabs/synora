eb29ce73eeb70296e8364e685ec97ed37fb8f2bd
David Jegat <david.jegat@knplabs.com>
2019-06-13 09:40:25 +0200

    ADd tone

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { render } from 'react-dom'
+import Tone from 'tone'
 
-const Synth = () =>
-  <div>
-    <p>Synthetizer goes here :)</p>
-  </div>
+const Synth = () => {
+  useEffect(() => {
+    //create a synth and connect it to the master output (your speakers)
+    var synth = new Tone.Synth().toMaster()
+
+    //play a middle 'C' for the duration of an 8th note
+    synth.triggerAttackRelease('C4', '8n')
+  }, [])
+
+  return (
+    <div>
+      <p>Synthetizer goes here :)</p>
+    </div>
+  )
+}
 
 const root = document.getElementById('root')
 
