c88d94573b70034990529f15eef3fff900638808
David Jegat <david.jegat@knplabs.com>
2019-06-13 09:44:11 +0200

    add styles

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React, { useEffect } from 'react'
 import { render } from 'react-dom'
 import Tone from 'tone'
+import './style.css'
 
 const Synth = () => {
-  useEffect(() => {
-    //create a synth and connect it to the master output (your speakers)
-    var synth = new Tone.Synth().toMaster()
-
-    //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease('C4', '8n')
-  }, [])
+  useEffect(setupTone, [])
 
   return (
     <div>
       <p>Synthetizer goes here :)</p>
     </div>
   )
+}
+
+const setupTone = () => {
+  //create a synth and connect it to the master output (your speakers)
+  var synth = new Tone.Synth().toMaster()
+
+  //play a middle 'C' for the duration of an 8th note
+  synth.triggerAttackRelease('C4', '8n')
 }
 
 const root = document.getElementById('root')
