790f58cd0481fbd88a38f0851f76dcbcc43cb006
neghmurken <pierre.bobiet@knplabs.com>
2019-06-13 11:28:29 +0200

    MIDI message handling

--- src/MidiInput.js
+++ src/MidiInput.js
@@ -1,26 +1,25 @@
 import { useEffect } from 'react'
 
 export const MidiInput = ({ onMessage = () => null }) => {
-  useEffect(setupMidiAccess(onMessage), [])
+  useEffect(() => {
+
+    if (!navigator.requestMIDIAccess) {
+      return console.error('No midi access')
+    }
+
+    navigator.requestMIDIAccess({
+        sysex: false // this defaults to 'false' and we won't be covering sysex in this article.
+    })
+    .then((midiAcess) => {
+      const inputs = midiAcess.inputs
+
+      inputs.forEach(input => {
+        input.onmidimessage = (message) => onMessage(message);
+      })
+    })
+    .catch(console.error)
+
+  }, [])
 
   return null;
-}
-
-const setupMidiAccess = onMessage => () => {
-  if (!navigator.requestMIDIAccess) {
-    return console.error('No midi access')
-  }
-
-  navigator.requestMIDIAccess({
-      sysex: false // this defaults to 'false' and we won't be covering sysex in this article.
-  })
-  .then(bindInputs(onMessage))
-  .catch(console.error)
-}
-
-const bindInputs = onMessage => (midiAcess) => {
-  const inputs = midiAcess.inputs
-  inputs.forEach(input => {
-    input.onmidimessage = onMessage;
-  })
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,91 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { render } from 'react-dom'
 import Tone from 'tone'
 import { MidiInput } from './MidiInput'
 import './style.css'
 
 const Synth = () => {
-  useEffect(setupTone, [])
+  const [tone, setSynth] = useState(null)
+  const [freq, setFreq] = useState(null)
+
+  useEffect(() => {
+    //create a synth and connect it to the master output (your speakers)
+    setSynth(new Tone.Synth().toMaster());
+  }, [])
+
+  useEffect(() => {
+    if (!navigator.requestMIDIAccess) {
+      return console.error('No midi access')
+    }
+
+    navigator.requestMIDIAccess({
+        sysex: false // this defaults to 'false' and we won't be covering sysex in this article.
+    })
+    .then((midiAcess) => {
+      const inputs = midiAcess.inputs
+
+      inputs.forEach(input => {
+        input.onmidimessage = (message) => {
+          const [ type, note, velocity ] = message.data
+
+          if (217 === type) {
+            return;
+          }
+
+          const frequency = Tone.Midi(note).toFrequency()
+
+          if (137 === type) {
+            setFreq(null)
+
+            return;
+          }
+
+          setFreq(frequency)
+        };
+      })
+    })
+    .catch(console.error)
+  }, [])
+
+  useEffect(() => {
+    if (!tone) {
+      return;
+    }
+
+    if (!freq) {
+      tone.triggerRelease()
+      return;
+    }
+
+    tone.triggerAttack(freq)
+  }, [freq])
 
   return (
     <div>
-      <MidiInput onMessage={console.warn} />
+      <div>{freq}</div>
       <p>Synthetizer goes here :)</p>
     </div>
   )
 }
 
-const setupTone = () => {
-  //create a synth and connect it to the master output (your speakers)
-  var synth = new Tone.Synth().toMaster()
+const handleMessage = (synth, message) => {
+  console.warn(synth);
+  if (synth === null) {
+    return;
+  }
 
-  //play a middle 'C' for the duration of an 8th note
-  synth.triggerAttackRelease('C4', '8n')
+  const [type, note, velocity] = message.data;
+
+  if (velocity <= 0) {
+    synth.triggerRelease();
+
+    return;
+  }
+
+  const frequency = Tone.Midi(note).toFrequency();
+  console.warn(frequency);
+
+  synth.triggerAttack(frequency);
 }
 
 const root = document.getElementById('root')
